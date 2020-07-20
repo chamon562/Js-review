3d7b6a357e76affb3ec5ba57edccc31b2c6a25bf
Chamon562 <Chamon562@gmail.com>
2020-07-20 14:34:14 -0700

    [functions] Add 3 functions

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Js-review
 This is my review of JavaScript. 
+
+##Examples of Data Types 
+``` javascript
+let name = 'Channee'
+let age = '35'
+let isCool = true;
+const myArray = ['Jim', 'Joe', 'Bob']
+
+const musicGenre = {
+    hipHop = 'GangStarr',
+    rap = 'Nipsey Hussle',
+    myMusic = ['Rap','RNB','Eletric','Rock','Foreign'],
+    subGenres: {
+        musicOne: 'House',
+        musicTwo: 'Trap',
+        musicTrhee: '80s Rock'
+
+    },
+    print function(){
+        console.log('My kind of music is' + this.musicGenre)
+    }
+
+}
+```
+
+## Examples of Functions
+```javascript
+
+//stnd function
+function addNumbers(num1, num2){
+    return num1 + num2;
+
+};
+
+//function expression 
+const multiplyNumbers = function(num1, num2){
+    return num1 * num2;
+};
+
+//arrow function 
+const subtractNumbers = (num1, num2) => {
+    return num1 - num2
+};
+```
--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let name = 'Channee'
 let age = '35'
 let isCool = true;
-const myArray = ['Keven', 'Malik', 'Asia']
+const myArray = ['Jim', 'Joe', 'Bob']
 console.log(name);
 console.log(age);
 console.log(isCool);
@@ -31,7 +31,66 @@ const tesla = {
 tesla.stockPrice = 1643.00;
 
 console.log(tesla);
-console.Log(tesla.vehicles.vehicleThree);
+// console.Log(tesla.vehicles.vehicleThree);
 console.log(tesla['vehicles']['vehicleThree']);
 
 tesla.print();
+
+// const musicGenre = {
+//     hipHop = 'GangStarr',
+//     rap = 'Nipsey Hussle',
+//     myMusic = ['Rap','RNB','Eletric','Rock','Foreign'],
+//     subGenres: {
+//         musicOne: 'House',
+//         musicTwo: 'Trap',
+//         musicTrhee: '80s Rock'
+
+//     },
+//     print: function(){
+//         console.log('My kind of music is' + this.musicGenre)
+//     }
+
+// }
+// can do forloop for each
+// function printFriends(array){
+//     array.forEach(friend =>{
+//         console.log(friend);
+//         array.forEach(function(element){
+
+//         })
+//     })
+// }
+
+
+function printName(element) {
+    console.log(element)
+}
+
+friends.forEach(function(friend){
+    printName(friend);
+});
+
+friends.forEach(friend =>{
+    printName(friend)
+})
+//invoke 
+printFriends(myArray);
+printFriends(tesla.foundingMembers);
+
+
+
+//stnd function
+function addNumbers(num1, num2){
+    return num1 + num2;
+
+};
+
+//function expression 
+const multiplyNumbers = function(num1, num2){
+    return num1 * num2;
+};
+
+//arrow function 
+const subtractNumbers = (num1, num2) => {
+    return num1 - num2
+};
