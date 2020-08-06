53bfebb808d5daed918d5ae702408974587c4006
Chamon562 <Chamon562@gmail.com>
2020-08-05 22:23:19 -0700

    [data] Add more data types

---
+++ standards.js
@@ -0,0 +1,12 @@
+console.log('standars javascript is connected');
+//lets makea  few data types
+//string
+let myName = 'Channee';
+console.log(myName);
+let myLastName = 'Math';
+console.log(myLastName);
+let myAge = 35;
+console.log(myAge);
+//array 
+const rollerBlades = ['USD', 'Razors', 'K2', 'Solomon', 'Roces'];
+console.log(rollerBlades);
